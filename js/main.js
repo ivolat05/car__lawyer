2e6af54f82a6d09f1e62e8a7e2ac4443c4eaf09f
andrei_labadaev <53191183+ivolat05@users.noreply.github.com>
2021-07-13 18:02:54 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,36 @@
-$(function(){
+$(function () {
+    $('.menu__burger').click((event) => {
+        $('.menu__burger').toggleClass('burger__active')
+
+    });
+
+    $('.services__slaider').slick({
+        infinite: true,
+        arrows: false,
+        dots: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+
+
+    var $status = $('.pagingInfo');
+    var $slickElement = $('.about__slaider');
+
+    $slickElement.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+        //currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
+        var i = (currentSlide ? currentSlide : 0) + 1;
+        var test = slick.slideCount;
+
+        $status.text(i + ' /' + test);
+    });
+
+    $('.about__slaider').slick({
+
+        prevArrow: '<button class="slick-btn slick-prev"><img src="images/arrow-left.png" alt="prev"></button>',
+        nextArrow: '<button class="slick-btn slick-next"><img src="images/arrow-right.png" alt="next"></button>',
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1
+    });
 
 });
